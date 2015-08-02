dad1f60d57b5a9393112dee252210db2b96ffbbe
raincoats <tomshaddock@outlook.com>
2015-08-02 13:44:04 +1000

    renaming functions etc

--- rainbow.py
+++ rainbow.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#--> you know what i hate? when i’m doing unicoding: utf-8 <--#
 import sys
 import getopt
-
-#reopen stdout unbuffered, stackoverflow 230751
-#sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+# from optparse import OptionParser
 
 def to_stdout(string):
 	sys.stdout.write(str(string))
@@ -14,22 +12,53 @@ def to_stderr(string):
 	sys.stderr.write(str(string))
 	sys.stderr.flush()
 
-def my_pain(eulogy):
-	pain = "\n\033[0m\033[7m" + '<< '
-	pain += str(eulogy)
-	pain += ' >>' + "\033[0m\n"
-	to_stderr(pain)
-	sys.exit(1)
-
+# remove all colours etc, reset terminal to default state.
 def ansi_reset():
 	to_stdout ("\033[0m\n")
 
+#similar to bye() but no debug and no exit, used to debug when colours go strange
+def my_pain(string):
+	to_stdout ("\033[0m")
+	to_stdout	("\033[7m" + string)
+	to_stdout ("\033[0m")
+
+# death function. writes some debug stuff to stderr in reverse video mode, and exits 1.
+def bye(eulogy):
+	ansi_reset()
+	to_strerr("\033[0m" + "\033[7m" + 
+		' ruh roh: --> ' + str(eulogy) + ' <-- ' +
+		"\033[0m" + "\n")
+	ansi_reset()
+	sys.exit(1)
+
+
+################## functions that deal with the program start here #########################
+
+def is_newline(character):
+	if character == "\n":
+			return True
+	else:
+		return False
+
+def change_direction(colour):
+	if colour >= colour_max:
+		return -1
+	elif colour <= colour_min:
+		return 1
+
 def rainbows_constructor(colour, mode, character):
+	if (colour < 0 or colour > 255):
+		if debug:
+			my_pain(character)
+			return
+		else:
+			colour = 255 if colour > 255 else colour
+			colour =   0 if colour <   0 else colour
+
 	h = colour / 43
 	f = colour - 43 * h
 	t = f * 255 / 43
 	q = 255 - t
-
 	ansi_code = "\033[%s;2;" % str(mode)
 
 	if h == 0:
@@ -45,24 +74,12 @@ def rainbows_constructor(colour, mode, character):
 	elif h == 5:
 		ansi_code += "255;0;%sm" % str(q)
 	else:
-		my_pain ( 'h='+str(h)+' colour='+str(colour)+' mode='+str(mode)+' character='+character )
-
-	ansi_code += str(character)
-	to_stdout (ansi_code)
-
-def is_newline(character):
-	if character == "\n" and reset_on_newlines == True:
-		ansi_reset()
-		return True
-	else:
-		return False
-
-def change_direction(colour):
-	if colour >= colour_max:
-		return -1
-	elif colour <= colour_min:
-		return 1
-
+		bye ( 'h='+str(h)+
+					' colour=' + str(colour)+
+					' mode='+str(mode)+
+					' character='+character)
+	
+	to_stdout (ansi_code + str(character))
 
 ########################################################
 def version():
@@ -84,13 +101,14 @@ If -f is not given, read from stdin.
   -r, --rate <rate>  Rate that it cycles through colours (from 0 to 255, default 4)
   -b, --bg           Background colour mode
       --fg           Foreground colour mode (default)
+  -s, --start        Colour to start at (0 to 255) [NOT IMPLEMENTED YET]
       --noreset      Don’t reset colour on newlines
-      --debug        Debug messages (instead of printing characters, it prints colour codes)
-  -q, --quiet        Hide error messages
+      --debug        Debug mode (instead of chars, it prints the colour codes)
   -h, --help         You know what this does (displays this help text)
+  -q, --quiet        Hide error messages
+  -t, --test         Tests your terminal’s colour capability
   -v, --verbose      Does nothing (in case you assumed it meant verbose, which is default)
   -V, --version      Display license & version info
-  -t, --test         Tests your terminal’s colour capability
 
 Examples:
     rainbow < /etc/passwd
@@ -103,22 +121,22 @@ Examples:
 
 ###################################################################################################
 
-debug=0
+debug=False
 colour=0
 direction=1 #1=increment, -1=decrement
-rate=2
-colour_min = 0
-colour_max = 255
+rate=4
+colour_min=(  0 + rate)
+colour_max=(255 - rate)
 mode=38 #38 for foreground, 48 for background
 reset_on_newlines=True
-quiet = False #by default anyway
+quiet=False #by default anyway
 
 ###################################################################################################
 #getting options
 try:
-	opts, args = getopt.gnu_getopt(sys.argv[1:], 'f:r:bnqhvVt',
+	opts, args = getopt.gnu_getopt(sys.argv[1:], 'f:r:bnqhvVts',
 									['file=', 'rate=', 'bg', 'fg', 'noreset', 'quiet', 'help',
-									'verbose', 'version', 'test'])
+									'verbose', 'version', 'test', 'start', 'debug'])
 
 	for opt, arg in opts:
 		if opt in ('-h', '--help'):
@@ -129,10 +147,15 @@ try:
 			mode = 48
 		elif opt in ('--fg'):
 			mode = 38
+		elif opt in ('-s', '--start'):
+			colour = int(arg)
 		elif opt in ('-f', '--file'):
 			infile = arg
 		elif opt in ('-r', '--rate'):
-			rate = arg
+			rate = int(arg)
+			colour_min=(  0 + rate)
+			colour_max=(255 - rate)
+
 		elif opt in ('-v', '--verbose'):
 			quiet = False
 		elif opt in ('-q', '--quiet'):
@@ -140,8 +163,7 @@ try:
 		elif opt in ('--noreset'):
 			reset_on_newlines = False
 		elif opt in ('--debug'):
-			debug = 1
-
+			debug = True
 		else:
 			usage()
 
@@ -155,32 +177,46 @@ try:
 	while True:
 		character = sys.stdin.read(1)
 		if not character:
+			if debug:
+				to_stderr("----------------------------------------------\n")
+				to_stderr("rate: %-5s min: %-5s max: %-5s" % (rate, colour_min, colour_max))
 			ansi_reset()
 			break
 
 		# first check if it's outside the 0-255 range
-		if not (colour_min < colour <= colour_max):
+		# if it is, then decrement/increment it
+		if (colour <= colour_min) or (colour >= colour_max):
 			direction = change_direction(colour)
 
 		# incrementing the colour
-		if direction == 1:
-			colour = colour + rate
-		elif direction == -1:
-			colour = colour - rate
+		colour = (colour + rate) if (direction == 1) else (colour - rate)
+
 
 		if debug:
 			if character == "\n":
 				to_stdout ("\n")
 			else:
 				rainbows_constructor(colour, mode, str(colour)+" " )
-		else:
 
-			if is_newline(character):
-				colour = 0
-			else:
-				#print colour and character
-				rainbows_constructor(colour, mode, character)
+		elif is_newline(character):
+			colour=0 if reset_on_newlines else colour
+			ansi_reset()
+		else:
+			rainbows_constructor(colour, mode, character) 			#print colour and character
+
 
 except KeyboardInterrupt:
 	sys.exit(0)
 
+"""
+	# if we're doing fg colours we can just reset and all that
+	# bg colours however still need a \033[0m at the end of each
+	# line, else you get ugly lines
+	if character == "\n" and reset_on_newlines == True:
+		if mode == 38: #fg
+			ansi_reset()
+			return True
+		elif mode == 48:
+	else:
+		return False
+"""
